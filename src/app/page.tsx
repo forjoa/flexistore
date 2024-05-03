75377f45162bf791fd172a4c93c29afe26803b6c
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-03 14:22:44 +0200

    fix: redirect api working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation'
 
 export default function C() {
-  if (typeof window !== 'undefined') {
+  if (typeof window == 'undefined') {
     redirect('/home')
   }
 
