9a80a3814cf0d3ea934b73ec40834d4efb62d306
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-05 00:53:59 +0200

    making use of getPresentationData function from server

--- src/components/home/index/Presentation.tsx
+++ src/components/home/index/Presentation.tsx
@@ -1,15 +1,16 @@
 import Image from 'next/image'
 import placeholder from '@/assets/placeholder.svg'
 import Link from 'next/link'
+import { getPresentationData } from '@/server/utils/getData'
 
-export default function Presentation() {
+export default async function Presentation() {
+  const { data } = await getPresentationData()
+
   return (
     <div className='flex h-[80vh] py-[100px] gap-10'>
       <div className='w-1/2 h-full flex flex-col justify-center items-start gap-5'>
-        <h1 className='text-7xl font-bold'>Flexible e-commerce</h1>
-        <p>
-          Add your own color palette, your own text and your own information.
-        </p>
+        <h1 className='text-7xl font-bold'>{data?.presentation_title}</h1>
+        <p>{data?.presentation_slogan}</p>
         <Link
           href={'/home/search'}
           className='w-auto bg-white text-black py-2 px-4 rounded'
