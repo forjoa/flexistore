816ae3fc3bb9835b39f55e28a6fd5e9e87aefeff
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-06 10:37:26 +0200

    create about us commponent

---
+++ src/components/home/index/AboutUs.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import placeholder from '@/assets/placeholder.svg'
+
+export default function AboutUs() {
+  return (
+    <div className='flex h-[80vh] py-[100px] gap-10'>
+      <div className='w-1/2 h-full flex flex-col justify-center items-start gap-5'>
+        <h1 className='text-7xl font-bold'>{'data?.presentation_title'}</h1>
+        <p>{'data?.presentation_slogan'}</p>
+        <Link
+          href={'/home/search'}
+          className='w-auto bg-white text-black py-2 px-4 rounded'
+        >
+          Shop now
+        </Link>
+      </div>
+      <div className='w-1/2'>
+        <Image
+          src={placeholder}
+          alt='Placeholder image'
+          className='h-full aspect-auto object-cover rounded'
+        />
+      </div>
+    </div>
+  )
+}
