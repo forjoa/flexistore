fd9ecd8630f9dc2f5fa5c294337f7388973f664e
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-06 10:53:11 +0200

    create function to get about data

--- src/server/utils/getData.ts
+++ src/server/utils/getData.ts
@@ -1,9 +1,18 @@
 import { supabase } from '../database/connection'
 
 export const getPresentationData = async (): Promise<{ data: PresentationData | null }> => {
-  const { data, error } = await supabase.from('general_data').select('*').eq('id', 1)
+  const { data, error } = await supabase.from('general_data').select('presentation_title, presentation_slogan').eq('id', 1)
 
   if (error) return { data: null }
  
   return { data: data[0] }
+}
+
+export const getAboutData = async (): Promise<{ data: AboutData | null }> => {
+  const { data, error } = await supabase.from('general_data').select('about_title, about_content').eq('id', 1)
+
+  if (error) return { data: null }
+
+  return { data: data[0] }
+
 }
