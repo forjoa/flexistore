dbb0c0803ac543ff9c3a742bfea966c5d069defe
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-06 15:36:39 +0200

    finish form logic to send data to db

--- src/components/home/index/ContactUs.tsx
+++ src/components/home/index/ContactUs.tsx
@@ -1,6 +1,19 @@
-import { sendContactMessage } from "@/server/utils/functions";
+import { sendContactMessage } from '@/server/utils/functions'
+import showToaster from '@/server/utils/clientFunctions'
 
 export default function ContactUs() {
+  const submitForm = async (data: FormData) => {
+    'use server'
+    const { success } = await sendContactMessage(data)
+
+    let s = success
+    let m = success
+      ? 'Message correctly sent!'
+      : 'Something went wrong while sending your message'
+
+    showToaster(s, m)
+  }
+
   return (
     <div className='flex h-[80vh] py-[100px] gap-10'>
       <div className='w-1/2 h-full flex flex-col justify-center items-start gap-5'>
@@ -11,13 +24,18 @@ export default function ContactUs() {
         </p>
       </div>
       <div className='w-1/2'>
-        <form className='h-full flex flex-col justify-evenly' action={sendContactMessage}>
+        <form
+          className='h-full flex flex-col justify-evenly'
+          action={submitForm}
+        >
           <div className='flex flex-col gap-2'>
             <label htmlFor='name'>Name</label>
             <input
               type='text'
               name='name'
               placeholder='Enter your name'
+              autoComplete='name'
+              id='name'
               className='p-4 rounded focus:outline-none focus:ring focus:border-blue-500 text-black'
             />
           </div>
@@ -27,6 +45,8 @@ export default function ContactUs() {
               type='email'
               name='email'
               placeholder='Enter your email'
+              autoComplete='email'
+              id='email'
               className='p-4 rounded focus:outline-none focus:ring focus:border-blue-500 text-black'
             />
           </div>
@@ -35,6 +55,7 @@ export default function ContactUs() {
             <textarea
               name='message'
               placeholder='Enter your message'
+              id='message'
               className='p-4 rounded focus:outline-none focus:ring focus:border-blue-500 text-black'
             ></textarea>
           </div>
