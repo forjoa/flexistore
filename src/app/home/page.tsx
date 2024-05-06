13d00c1df895fe86679d9a5a613be76a3fe6bc55
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-06 10:38:24 +0200

    add about us component to home index

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,12 @@
 import Main from '@/components/home/globals/Main'
+import AboutUs from '@/components/home/index/AboutUs'
 import Presentation from '@/components/home/index/Presentation'
 
 export default function Home() {
   return (
     <Main>
       <Presentation />
+      <AboutUs />
     </Main>
   )
 }
