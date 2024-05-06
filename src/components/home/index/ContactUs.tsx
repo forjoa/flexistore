5f49d890d1b7f18a0495a126dd7fb9ccd418eb0b
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-06 10:58:34 +0200

    create contact us component

---
+++ src/components/home/index/ContactUs.tsx
@@ -0,0 +1,13 @@
+export default function ContactUs() {
+  return (
+    <div className='flex h-[80vh] py-[100px] gap-10'>
+      <div className='w-1/2 h-full flex flex-col justify-center items-start gap-5'>
+        <h1 className='text-5xl font-bold'>Get in touch with us</h1>
+        <p>
+          Have a question or feedback? We&apos;d love to hear from you. Fill out
+          the form below and we&apos;ll get back to you as soon as possible.
+        </p>
+      </div>
+    </div>
+  )
+}
