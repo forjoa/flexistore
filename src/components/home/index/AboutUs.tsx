433349dd5abcbe51c87861eab4b3b590195e17cd
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-06 10:42:08 +0200

    visible styles finished

--- src/components/home/index/AboutUs.tsx
+++ src/components/home/index/AboutUs.tsx
@@ -1,26 +1,28 @@
-import Link from 'next/link'
 import Image from 'next/image'
 import placeholder from '@/assets/placeholder.svg'
 
 export default function AboutUs() {
   return (
     <div className='flex h-[80vh] py-[100px] gap-10'>
-      <div className='w-1/2 h-full flex flex-col justify-center items-start gap-5'>
-        <h1 className='text-7xl font-bold'>{'data?.presentation_title'}</h1>
-        <p>{'data?.presentation_slogan'}</p>
-        <Link
-          href={'/home/search'}
-          className='w-auto bg-white text-black py-2 px-4 rounded'
-        >
-          Shop now
-        </Link>
-      </div>
       <div className='w-1/2'>
         <Image
           src={placeholder}
           alt='Placeholder image'
           className='h-full aspect-auto object-cover rounded'
         />
+      </div>
+      <div className='w-1/2 h-full flex flex-col justify-center items-start gap-5'>
+        <h1 className='text-5xl font-bold'>About our e-commerce</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor
+          debitis veritatis tempore, cum in pariatur necessitatibus optio nam
+          corrupti omnis natus, quasi consequatur fuga doloribus ad, nostrum
+          odio asperiores officiis. Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Blanditiis sunt corporis, perferendis deleniti
+          cumque suscipit, voluptate accusantium dolorem est consectetur quasi.
+          Explicabo fuga officiis nostrum aspernatur, amet optio voluptatibus
+          maiores!
+        </p>
       </div>
     </div>
   )
