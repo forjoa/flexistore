d95b65bbf75ddbd02b936e53d86154b3b17cb4a9
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-06 15:06:31 +0200

    complete function to send data to db

--- src/server/utils/functions.ts
+++ src/server/utils/functions.ts
@@ -1,10 +1,21 @@
+import { supabase } from '../database/connection'
+
 export async function sendContactMessage(data: FormData) {
-    'use server'
+  'use server'
   const name = data.get('name')
   const email = data.get('email')
   const message = data.get('message')
 
-  console.log(name, email, message)
+  try {
+    const { error } = await supabase
+      .from('contact_messages')
+      .insert({ name: name, email: email, message: message })
+
+    if (error) return { success: false }
+
+  } catch (er: any) {
+    return { success: false }
+  }
 
   return { success: true }
 }
