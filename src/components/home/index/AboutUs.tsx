b287f11d00fbfe1d63b198d67a76a213bf539e20
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-07 13:20:34 +0200

    add mobile version for about us component

--- src/components/home/index/AboutUs.tsx
+++ src/components/home/index/AboutUs.tsx
@@ -6,19 +6,17 @@ export default async function AboutUs() {
   const { data } = await getAboutData()
 
   return (
-    <div className='flex h-[80vh] py-[100px] gap-10'>
-      <div className='w-1/2'>
+    <div className='flex flex-col-reverse mt-16 md:mt-0 md:flex-row md:h-[80vh] md:py-[100px] gap-10'>
+      <div className='w-full md:w-1/2'>
         <Image
           src={placeholder}
           alt='Placeholder image'
           className='h-full aspect-auto object-cover rounded'
         />
       </div>
-      <div className='w-1/2 h-full flex flex-col justify-center items-start gap-5'>
+      <div className='w-full md:w-1/2 h-full flex flex-col justify-center items-start gap-5'>
         <h1 className='text-5xl font-bold'>{data?.about_title}</h1>
-        <p>
-          {data?.about_content}
-        </p>
+        <p>{data?.about_content}</p>
       </div>
     </div>
   )
