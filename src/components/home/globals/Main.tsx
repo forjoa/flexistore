b39e8084da97b0625d9c5bfbc310ed2de3d34adf
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-07 13:14:53 +0200

    add mq for global component Main

--- src/components/home/globals/Main.tsx
+++ src/components/home/globals/Main.tsx
@@ -1,7 +1,7 @@
 export default function Main({ children }: { children: React.ReactNode }) {
   return (
     <main className='w-full mt-20 flex justify-center'>
-      <div className='w-[70%]'>{children}</div>
+      <div className='w-[80%] md:w-[70%]'>{children}</div>
     </main>
   )
 }
