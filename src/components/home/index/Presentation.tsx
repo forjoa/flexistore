e3c6495c5dfa14642aabd0536940395f082ae58d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-07 13:15:34 +0200

    add mobile version for presentation

--- src/components/home/index/Presentation.tsx
+++ src/components/home/index/Presentation.tsx
@@ -7,8 +7,8 @@ export default async function Presentation() {
   const { data } = await getPresentationData()
 
   return (
-    <div className='flex h-[80vh] py-[100px] gap-10'>
-      <div className='w-1/2 h-full flex flex-col justify-center items-start gap-5'>
+    <div className='flex flex-col gap-5 md:flex md:h-[80vh] md:py-[100px] md:gap-10'>
+      <div className='w-full md:w-1/2 h-full flex flex-col justify-center items-start gap-5'>
         <h1 className='text-7xl font-bold'>{data?.presentation_title}</h1>
         <p>{data?.presentation_slogan}</p>
         <Link
@@ -18,7 +18,7 @@ export default async function Presentation() {
           Shop now
         </Link>
       </div>
-      <div className='w-1/2'>
+      <div className='w-full md:w-1/2'>
         <Image
           src={placeholder}
           alt='Placeholder image'
