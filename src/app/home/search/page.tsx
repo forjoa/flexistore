dfe27b678fe806ea5e8c1b8c9a8a0f79a03fb62c
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-08 16:46:51 +0200

    update search input styles

--- src/app/home/search/page.tsx
+++ src/app/home/search/page.tsx
@@ -45,7 +45,7 @@ export default async function Search() {
               <div className='relative flex items-center'>
                 <SearchIcon className='absolute left-2.5 h-4 w-4 text-gray-500 dark:text-gray-400' />
                 <input
-                  className='w-full bg-white shadow-none appearance-none pl-8 md:w-2/3 lg:w-1/3 dark:bg-gray-950'
+                  className='w-full shadow-none appearance-none pl-8 md:w-2/3 lg:w-1/3 bg-transparent outline-none'
                   placeholder='Search products...'
                   type='search'
                 />
