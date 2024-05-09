494cbcae91d5ad018361f3f6ff65af5bdf9b7e84
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-09 15:36:51 +0200

    create and add file for tsx icons

--- src/app/home/search/page.tsx
+++ src/app/home/search/page.tsx
@@ -6,6 +6,7 @@ import placeholder from '@/assets/placeholder.svg'
 import { getCategories, getProducts } from '@/server/utils/getData'
 import { useState, useEffect } from 'react'
 import { Toaster, toast } from 'sonner'
+import { SearchIcon } from '@/assets/icons'
 
 export default function Search() {
   const [categories, setCategories] = useState<Category[]>([])
@@ -104,25 +105,5 @@ export default function Search() {
         </div>
       </div>
     </Main>
-  )
-}
-
-function SearchIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-    >
-      <circle cx='11' cy='11' r='8' />
-      <path d='m21 21-4.3-4.3' />
-    </svg>
   )
 }
