b45efd0464958fde75e0c841d5c7ff7db8431b50
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-13 23:16:34 +0200

    updaet input type radio styles

--- src/components/home/search/CategoryFilter.tsx
+++ src/components/home/search/CategoryFilter.tsx
@@ -13,13 +13,14 @@ const CategoryFilter = ({
       <div className='space-y-2'>
         {categories.length === 0 && <p>Loading categories...</p>}
         {categories.map((category, index) => (
-          <label key={index} className='flex items-center gap-2 text-sm'>
+          <label key={index} className='flex items-center gap-2 text-sm cursor-pointer'>
             <input
               type='radio'
               id={`${category.category_id}`}
               name='category'
               onClick={() => setFilterCategory(category.category_id)}
               checked={filterCategory === category.category_id}
+              className='appearance-none border-2 border-white h-3 w-3 rounded checked:bg-blue-500 checked:text-blue-500 checked:border-blue-500'
             />
             {category.name}
           </label>
