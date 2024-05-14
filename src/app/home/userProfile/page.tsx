e670f5ab292305a23f51a3fa81ba569f3e62bd8b
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-14 12:55:59 +0200

    add redirection when user is not logged in

--- src/app/home/userProfile/page.tsx
+++ src/app/home/userProfile/page.tsx
@@ -1,6 +1,18 @@
+'use client'
 import Main from '@/components/home/globals/Main'
+import { useRouter } from 'next/navigation'
 
 export default function UserProfile() {
+  const router = useRouter()
+  const user: Client = sessionStorage.getItem('user')
+    ? JSON.parse(sessionStorage.getItem('user') || '')
+    : null
+
+  if (!user) {
+    router.push('/home')
+    return
+  }
+
   return (
     <Main>
       <p>User profile</p>
