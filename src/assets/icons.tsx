caa247177ca3ccde4e54b2a976b62adf30783d87
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-14 23:27:33 +0200

    add new icons

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -17,3 +17,93 @@ export function SearchIcon(props: any) {
     </svg>
   )
 }
+
+export function LocateIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <line x1='2' x2='5' y1='12' y2='12' />
+      <line x1='19' x2='22' y1='12' y2='12' />
+      <line x1='12' x2='12' y1='2' y2='5' />
+      <line x1='12' x2='12' y1='19' y2='22' />
+      <circle cx='12' cy='12' r='7' />
+    </svg>
+  )
+}
+
+export function MailboxIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path d='M22 17a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9.5C2 7 4 5 6.5 5H18c2.2 0 4 1.8 4 4v8Z' />
+      <polyline points='15,9 18,9 18,11' />
+      <path d='M6.5 5C9 5 11 7 11 9.5V17a2 2 0 0 1-2 2v0' />
+      <line x1='6' x2='7' y1='10' y2='10' />
+    </svg>
+  )
+}
+
+export function PhoneIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path d='M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z' />
+    </svg>
+  )
+}
+
+export function CalendarIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='#ffffff'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className='icon icon-tabler icons-tabler-outline icon-tabler-calendar'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z' />
+      <path d='M16 3v4' />
+      <path d='M8 3v4' />
+      <path d='M4 11h16' />
+      <path d='M11 15h1' />
+      <path d='M12 15v3' />
+    </svg>
+  )
+}
