aa51c11ba2e283162e0784006c482138d6da09ad
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-15 17:25:09 +0200

    add current route in user navigation to know where he is

--- src/components/home/userProfile/UserNav.tsx
+++ src/components/home/userProfile/UserNav.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function UserNav() {
+  const currentRoute = usePathname().split('/')[usePathname().split('/').length - 1]
+  
   return (
     <div className='w-full h-auto my-4'>
       <nav className='h-auto'>
@@ -8,7 +11,7 @@ export default function UserNav() {
           <li>
             <Link
               href='/home/userProfile'
-              className='py-2 px-4 rounded transition-all hover:bg-white hover:bg-opacity-50'
+              className={`py-2 px-4 rounded transition-all hover:bg-white hover:bg-opacity-50 ${currentRoute === 'userProfile' ? 'bg-white bg-opacity-30' : ''}`}
             >
               General information
             </Link>
@@ -16,7 +19,7 @@ export default function UserNav() {
           <li>
             <Link
               href='/home/userProfile/orders'
-              className='py-2 px-4 rounded transition-all hover:bg-white hover:bg-opacity-50'
+              className={`py-2 px-4 rounded transition-all hover:bg-white hover:bg-opacity-50 ${currentRoute === 'orders' ? 'bg-white bg-opacity-30' : ''}`}
             >
               Orders
             </Link>
