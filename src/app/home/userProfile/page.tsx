bf067d8f599659f362ae6909d5cb6bbb1c0788bb
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-05-15 18:02:12 +0200

    ui form finished (todo: update user state)

--- src/app/home/userProfile/page.tsx
+++ src/app/home/userProfile/page.tsx
@@ -1,10 +1,27 @@
+'use client'
+import { useEffect, useState } from 'react'
+
 export default function UserProfile() {
+  const [user, setUser] = useState<Client>()
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setUser(
+        window.sessionStorage.getItem('user')
+          ? JSON.parse(window.sessionStorage.getItem('user') || '')
+          : null
+      )
+    }
+  }, [])
+
+  console.log(user)
+  
   return (
-    <div className='w-full max-w-2xl shadow-md rounded-lg overflow-hidden'>
+    <div className='w-full max-w-2xl shadow-md rounded-lg overflow-hidden block m-auto'>
       <div className='p-4'>
         <p>Update your personal details.</p>
       </div>
-      <div className='grid gap-4 p-4'>
+      <form className='grid gap-4 p-4'>
         <div className='grid grid-cols-2 gap-4'>
           <div className='space-y-2'>
             <label htmlFor='name' className='block font-medium'>
@@ -15,13 +32,11 @@ export default function UserProfile() {
               className='w-full border border-gray-800 bg-transparent p-4 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500'
               type='text'
               placeholder='Enter your name'
+              value={user?.name}
             />
           </div>
           <div className='space-y-2'>
-            <label
-              htmlFor='lastname'
-              className='block font-medium'
-            >
+            <label htmlFor='lastname' className='block font-medium'>
               Last Name
             </label>
             <input
@@ -29,6 +44,7 @@ export default function UserProfile() {
               className='w-full border border-gray-800 bg-transparent p-4 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500'
               type='text'
               placeholder='Enter your last name'
+              value={user?.lastname}
             />
           </div>
         </div>
@@ -40,6 +56,7 @@ export default function UserProfile() {
             id='address'
             className='w-full border border-gray-800 bg-transparent p-4 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 min-h-32'
             placeholder='Enter your address'
+            value={user?.address}
           ></textarea>
         </div>
         <div className='grid grid-cols-2 gap-4'>
@@ -52,24 +69,9 @@ export default function UserProfile() {
               className='w-full border border-gray-800 bg-transparent p-4 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500'
               type='email'
               placeholder='Enter your email'
+              value={user?.email}
             />
           </div>
-          <div className='space-y-2'>
-            <label
-              htmlFor='password'
-              className='block font-medium'
-            >
-              Password
-            </label>
-            <input
-              id='password'
-              className='w-full border border-gray-800 bg-transparent p-4 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500'
-              type='password'
-              placeholder='Enter your password'
-            />
-          </div>
-        </div>
-        <div className='grid grid-cols-2 gap-4'>
           <div className='space-y-2'>
             <label htmlFor='phone' className='block font-medium'>
               Phone
@@ -78,34 +80,20 @@ export default function UserProfile() {
               id='phone'
               className='w-full border border-gray-800 bg-transparent p-4 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500'
               placeholder='Enter your phone number'
-            />
-          </div>
-          <div className='space-y-2'>
-            <label
-              htmlFor='created_at'
-              className='block font-medium'
-            >
-              Created At
-            </label>
-            <input
-              id='created_at'
-              className='w-full border border-gray-800 bg-transparent p-4 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500'
-              type='date'
+              value={user?.phone}
             />
           </div>
         </div>
         <div className='grid grid-cols-2 gap-4'>
           <div className='space-y-2'>
-            <label
-              htmlFor='birth_date'
-              className='block font-medium'
-            >
+            <label htmlFor='birth_date' className='block font-medium'>
               Birth Date
             </label>
             <input
               id='birth_date'
               className='w-full border border-gray-800 bg-transparent p-4 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500'
               type='date'
+              value={user?.birth_date?.toString()}
             />
           </div>
           <div className='space-y-2'>
@@ -116,15 +104,18 @@ export default function UserProfile() {
               id='dni'
               className='w-full border border-gray-800 bg-transparent p-4 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500'
               placeholder='Enter your DNI'
+              value={user?.dni}
             />
           </div>
         </div>
-      </div>
-      <div className='px-4 py-3'>
-        <button className='ml-auto px-4 py-2 bg-blue-500 text-white rounded-md font-semibold'>
-          Save Changes
-        </button>
-      </div>
+        <div className='px-4 py-3'>
+          <input
+            type='submit'
+            value={'Save changes'}
+            className='ml-auto px-4 py-2 bg-blue-500 text-white rounded-md font-semibold cursor-pointer'
+          />
+        </div>
+      </form>
     </div>
   )
 }
